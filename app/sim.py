f66280068c59ae8c7ab1886de97654396bcd7f3b
katzjm <katzjm@uw.edu>
2018-11-12 23:06:40 -0800

    Successfull graph drawing with vis.js

--- app/sim.py
+++ app/sim.py
@@ -2,9 +2,9 @@ import functools
 import roadrunner as rr
 from io import StringIO
 import numpy
-
+import tesbml
 from flask import (
-	Blueprint, flash, g, redirect, render_template, request, session, url_for, json
+	Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 
 bp = Blueprint('sim', __name__)
@@ -23,11 +23,39 @@ def run_sim():
 
 @bp.route('/upload', methods=['POST'])
 def upload():
-	sbml = request.files['sbml']
-	r = rr.load(sbml)
+	sbmlfile = request.files['sbml']
+	sbml = sbmlfile.read().decode('UTF-8')
+	sbmldoc = tesbml.readSBMLFromString(sbml)
+	sbmlmodel = sbmldoc.getModel()
 
 	model = dict()
-	model['nodes'] = r.getBoundarySpeciesIds()
+	model['nodes'] = list()
+	model['edges'] = list()
+	nodeid = 0
+	ids = dict()
+	for i, reacref in enumerate(sbmlmodel.getListOfReactions()):
+		reaction = reacref.getId()
+		if reaction not in ids:
+			ids[reaction] = nodeid
+			nodeid += 1
+		model['nodes'].append({'id': ids[reaction], 'size': 0.5})
+		for specref in reacref.getListOfReactants():
+			species = specref.getSpecies()
+			if species not in ids:
+				model['nodes'].append({'id': nodeid, 'label': species})
+				ids[species] = nodeid
+				nodeid += 1
+			model['edges'].append({'from': ids[species], 'to': ids[reaction]})
+		for specref in reacref.getListOfProducts():
+			species = specref.getSpecies()
+			if species not in ids:
+				model['nodes'].append({'id': nodeid, 'label': species})
+				ids[species] = nodeid
+				nodeid += 1
+			model['edges'].append({'from': ids[reaction], 'to': ids[species], 'arrows': 'to'})
+
+	return jsonify(model)
+
 	
 
 
--- app/static/js/drawgraph.js
+++ app/static/js/drawgraph.js
@@ -15,33 +15,58 @@
 		}
 	}
 
-	function handleGraphJSON(data){
-		nodes = [
-			{id: 1,  value: 2,  label: 'Algie' },
-			{id: 2,  value: 31, label: 'Alston'},
-			{id: 3,  value: 12, label: 'Barney'},
-			{id: 4,  value: 16, label: 'Coley' },
-			{id: 5,  value: 17, label: 'Grant' },
-			{id: 6,  value: 15, label: 'Langdon'},
-			{id: 7,  value: 6,  label: 'Lee'},
-			{id: 8,  value: 5,  label: 'Merlin'},
-			{id: 9,  value: 30, label: 'Mick'},
-			{id: 10, value: 18, label: 'Tod'},
-		];
+	function handleGraphJSON(json){
+		// console.log(json);
+		// nodes = [
+		// 	{id: 1,  value: 2,  label: 'Algie' },
+		// 	{id: 2,  value: 31, label: 'Alston'},
+		// 	{id: 3,  value: 12, label: 'Barney'},
+		// 	{id: 4,  value: 16, label: 'Coley' },
+		// 	{id: 5,  value: 17, label: 'Grant' },
+		// 	{id: 6,  value: 15, label: 'Langdon'},
+		// 	{id: 7,  value: 6,  label: 'Lee'},
+		// 	{id: 8,  value: 5,  label: 'Merlin'},
+		// 	{id: 9,  value: 30, label: 'Mick'},
+		// 	{id: 10, value: 18, label: 'Tod'},
+		// ];
 
-		edges = [
-			{from: 2, to: 8, value: 3, title: '3 emails per week'},
-			{from: 2, to: 9, value: 5, title: '5 emails per week'},
-			{from: 2, to: 10,value: 1, title: '1 emails per week'},
-			{from: 4, to: 6, value: 8, title: '8 emails per week'},
-			{from: 5, to: 7, value: 2, title: '2 emails per week'},
-			{from: 4, to: 5, value: 1, title: '1 emails per week'},
-			{from: 9, to: 10,value: 2, title: '2 emails per week'},
-			{from: 2, to: 3, value: 6, title: '6 emails per week'},
-			{from: 3, to: 9, value: 4, title: '4 emails per week'},
-			{from: 5, to: 3, value: 1, title: '1 emails per week'},
-			{from: 2, to: 7, value: 4, title: '4 emails per week'}
-		];
+		// edges = [
+		// 	{from: 2, to: 8, value: 3, title: '3 emails per week'},
+		// 	{from: 2, to: 9, value: 5, title: '5 emails per week'},
+		// 	{from: 2, to: 10,value: 1, title: '1 emails per week'},
+		// 	{from: 4, to: 6, value: 8, title: '8 emails per week'},
+		// 	{from: 5, to: 7, value: 2, title: '2 emails per week'},
+		// 	{from: 4, to: 5, value: 1, title: '1 emails per week'},
+		// 	{from: 9, to: 10,value: 2, title: '2 emails per week'},
+		// 	{from: 2, to: 3, value: 6, title: '6 emails per week'},
+		// 	{from: 3, to: 9, value: 4, title: '4 emails per week'},
+		// 	{from: 5, to: 3, value: 1, title: '1 emails per week'},
+		// 	{from: 2, to: 7, value: 4, title: '4 emails per week'}
+		// ];
+
+  //     // Instantiate our network object.
+		// var container = document.getElementById('network');
+		// var data = {
+		// 	nodes: nodes,
+		// 	edges: edges
+		// };
+		// var options = {
+		// 	nodes: {
+		// 		shape: 'dot',
+		// 		scaling:{
+		// 			label: {
+		// 				min:8,
+		// 				max:20
+		// 			}
+		// 		}
+		// 	}
+		// };
+		// network = new vis.Network(container, data, options);
+
+		nodes = json['nodes']
+		edges = json['edges']
+		console.log(nodes)
+		console.log(edges)
 
       // Instantiate our network object.
 		var container = document.getElementById('network');
@@ -85,6 +110,8 @@
 				.then(JSON.parse)
 				.then(handleGraphJSON)
 				.catch(console.log);
+
+			uploadButton.innerHTML = 'Upload';
 		});
 	}
 })();
