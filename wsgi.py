a9c3375bcc1a5d517be685ae60db6b3d8073d954
katzjm <katzjm@uw.edu>
2019-05-20 09:10:30 -0700

    raise Exception

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from app import create_app
 from flask_socketio import SocketIO
 
+import os; raise Exception(os.getcwd())
 app = create_app()
 socketio = SocketIO(app)
 
 if __name__ == '__main__':
-	import os; raise Exception(os.getcwd())
 	socketio.run(app)
